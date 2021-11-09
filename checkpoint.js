a2f6aaeaf393fffad98feba3d54363707645657d
Leandro Vieira <91228820+Lecovin@users.noreply.github.com>
2021-11-09 20:05:51 -0300

    Add files via upload
    Checkpoint Programação Imperativa 09/11/2021

---
+++ checkpoint.js
@@ -0,0 +1,22 @@
+let pipoca = 10
+let macarrão = 8
+let carne = 15
+let feijão = 12
+let brigadeiro = 8
+
+function cozinhou(pipoca){
+}
+
+if (pipoca == 10*2){
+    console.log("Comida Queimou")
+}
+
+else if (pipoca < 10){
+    console.log ("Tempo insuficiente")
+}
+else if (pipoca ==10*3){
+    console.log("Kabum")
+}
+else {
+    console.log("Prato pronto, bom apetite!!!")
+}
