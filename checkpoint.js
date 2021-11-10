873fddab3abd9f73c3aee4934a65ba10e3e5327c
Leandro Vieira <91228820+Lecovin@users.noreply.github.com>
2021-11-09 21:50:59 -0300

    Add files via upload
    Versão feita em aula

--- checkpoint.js
+++ checkpoint.js
@@ -5,6 +5,7 @@ let feijão = 12
 let brigadeiro = 8
 
 function cozinhou(pipoca){
+
 }
 
 if (pipoca == 10*2){
@@ -15,6 +16,79 @@ else if (pipoca < 10){
     console.log ("Tempo insuficiente")
 }
 else if (pipoca ==10*3){
+    console.log("Kabum")
+}
+else {
+    console.log("Prato pronto, bom apetite!!!")
+}
+
+
+function cozinhou(macarrao){
+
+}
+
+if (macarrão == 8*2){
+    console.log("Comida Queimou")
+}
+
+else if (macarrão < 8){
+    console.log ("Tempo insuficiente")
+}
+else if (macarrão ==8*3){
+    console.log("Kabum")
+}
+else {
+    console.log("Prato pronto, bom apetite!!!")
+}
+
+function cozinhou(carne){
+
+}
+
+if (carne == 15*2){
+    console.log("Comida Queimou")
+}
+
+else if (carne < 15){
+    console.log ("Tempo insuficiente")
+}
+else if (carne ==15*3){
+    console.log("Kabum")
+}
+else {
+    console.log("Prato pronto, bom apetite!!!")
+}
+
+function cozinhou(feijão){
+
+}
+
+if (feijão == 12*2){
+    console.log("Comida Queimou")
+}
+
+else if (feijão < 10){
+    console.log ("Tempo insuficiente")
+}
+else if (feijão ==10*3){
+    console.log("Kabum")
+}
+else {
+    console.log("Prato pronto, bom apetite!!!")
+}
+
+function cozinhou(brigadeiro){
+
+}
+
+if (pipoca == 8*2){
+    console.log("Comida Queimou")
+}
+
+else if (pipoca < 8){
+    console.log ("Tempo insuficiente")
+}
+else if (pipoca ==8*3){
     console.log("Kabum")
 }
 else {
